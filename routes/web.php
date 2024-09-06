8a8d49c2ec8a9b2310229f6da77e664059e0901b
enjelianna <angeliarazali@gmail.com>
2024-09-06 10:31:43 +0700

    (JS2)Praktikum1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,41 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+   });
+
+Route::get('/world', function () {
+    return 'World';
+   });
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+   });
+
+Route::get('/about', function () {
+    return ('NIM: 2241760016, NAMA: ANGELIA ANNA PUTRI R');
+   });
+
+   Route::get('/user/{Angelia}', function ($name) {
+    return 'Nama Saya '.$name;
+   });
+
+   Route::get('/posts/{post}/comments/{comment}', function
+    ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-:" .$commentId;
+   });
+
+   Route::get('/Articles/{Id}', function ($Id) {
+   return 'Halaman Artikel dengan ID - '.$Id;
+  });
+
+  Route::get('/User/{name?}', function ($name=null) {
+    return 'Nama Saya '.$name;
+   });
+
+   Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
+
